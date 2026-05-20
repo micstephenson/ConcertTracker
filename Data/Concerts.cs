3c3875a535f034aec58e90650d91080cf1a32c85
micstephenson <159960082+micstephenson@users.noreply.github.com>
2026-05-20 14:05:23 +0100

    Add project files.

---
+++ Data/Concerts.cs
@@ -0,0 +1,9 @@
+﻿namespace ConcertTrackerBlazorHybridApp.Data;
+
+public class Concerts
+{
+    public string Artist { get; set; }
+    public string Location { get; set; }
+    public DateTime Date { get; set; }
+    public bool Attended { get; set; } = false;
+}
